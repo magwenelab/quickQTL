0348741995e99aa23a8c4816e533fe0cceab1fd3
Paul Magwene <paul.magwene@duke.edu>
2022-02-22 19:16:34 -0500

    renaming

--- quickQTL.py
+++ quickQTL.py
@@ -1,49 +1,94 @@
 #!/usr/bin/env python
 
-
 from itertools import cycle
 
 import numpy as np
 from scipy import stats
 import pandas as pd
+import matplotlib
 from matplotlib import pyplot as plt
 
 import click
 
+from pandarallel import pandarallel
 
-def calculate_association_stats(genotypes, phenotypes, focal_phenotype=None):
-
-    phenotypes = phenotypes.set_index("Sample_Name")
-    if focal_phenotype is None:
-        focal_phenotype = phenotypes.columns[0]
-
-    qtl_stats = []
-
-    for i, site in genotypes.iterrows():
-        chrom_coord_pval = site[:2].values.tolist()
-        lgeno = site[2:]
-        has_geno0 = lgeno.index[lgeno == 0]
-        has_geno1 = lgeno.index[lgeno == 1]
-        if (len(has_geno0) < 2) or (len(has_geno1) < 2):
-            chrom_coord_pval.append(np.NaN)
-            qtl_stats.append(chrom_coord_pval)
-            continue
-        pheno0 = phenotypes.loc[has_geno0, focal_phenotype]
-        pheno1 = phenotypes.loc[has_geno1, focal_phenotype]
-        tstat, pvalue = stats.ttest_ind(pheno0, pheno1)
-        chrom_coord_pval.append(pvalue)
-        qtl_stats.append(chrom_coord_pval)
-
-    return pd.DataFrame(qtl_stats, columns=("Chromosome", "Coordinate", "p_value"))
+pandarallel.initialize(verbose=0)
 
 
-def plot_log10pval(association_stats, chroms):
+@click.group()
+def cli():
+    pass
+
+
+def load_files(genotypefile, phenotypefile):
+    with open(genotypefile, "r") as gfile, open(phenotypefile, "r") as pfile:
+
+        genotypes_df = pd.read_csv(gfile, index_col=("Chromosome", "Coordinate"))
+        phenotypes_df = pd.read_csv(pfile, index_col="Sample_Name")
+
+        # reindexing makes phenotype sample order conform to genotype sample order
+        # sample names in genotype header that don't exist get a NaN in the
+        # phenotype columns; sample names in phenotypes that don't exist in
+        # genotype header are dropped
+        phenotypes_df = phenotypes_df.reindex(genotypes_df.columns)
+
+        return genotypes_df, phenotypes_df
+
+
+assoc_test_dict = {
+    "anova": stats.f_oneway,
+    "alexander": stats.alexandergovern,
+    "kruskal": stats.kruskal,
+}
+
+
+def assoc_by_genotype(genorow, phenodf, focalpheno, genos=(0, 1), test="anova"):
+    groups = [
+        phenodf[genorow == i][focalpheno].dropna().values.flatten() for i in genos
+    ]
+    statistic, pval = assoc_test_dict[test](*groups)
+    return statistic, pval
+
+
+def assoc_test_pandarallel(genodf, phenodf, focalpheno, genos=(0, 1), test="anova"):
+    assoc = genodf.parallel_apply(
+        assoc_by_genotype,
+        args=(phenodf, focalpheno, genos, test),
+        axis=1,
+        result_type="expand",
+    )
+    assoc.columns = ("statistic", "Pvalue")
+    return assoc
+
+
+def assoc_test_pandas(genodf, phenodf, focalpheno, genos=(0, 1), test="anova"):
+    assoc = genodf.apply(
+        assoc_by_genotype,
+        args=(phenodf, focalpheno, genos, test),
+        axis=1,
+        result_type="expand",
+    )
+    assoc.columns = ("F", "Pvalue")
+    return assoc
+
+
+@cli.command()
+@click.option("--output", "-o", type=str, default="")
+@click.argument("statfile", type=click.File("r"))
+@click.argument("chromfile", type=click.File("r"))
+def plot(statfile, chromfile, output):
+    association_stats = pd.read_csv(statfile)
+    chroms = pd.read_csv(chromfile)
+
     chroms = chroms.set_index("Chromosome")
     chroms["Offset"] = [0] + chroms.Length[:-1].cumsum().values.tolist()
     chroms["Midpoint"] = (chroms.Length + 2 * chroms.Offset) / 2
     chroms["Endpoint"] = chroms.Length + chroms.Offset
 
     grouped_stats = association_stats.groupby("Chromosome")
+
+    if bool(output):
+        matplotlib.use("agg")
 
     colorcycle = cycle(plt.cm.Dark2.colors)
     fig, ax = plt.subplots(1, 1)
@@ -52,7 +97,7 @@ def plot_log10pval(association_stats, chroms):
         grp = grouped_stats.get_group(chrom)
         ax.plot(
             grp.Coordinate + chroms.loc[chrom, "Offset"],
-            -np.log10(grp.p_value),
+            -np.log10(grp.Pvalue),
             markersize=2,
             marker="o",
             linestyle="None",
@@ -61,39 +106,36 @@ def plot_log10pval(association_stats, chroms):
         )
     ax.set_xticks(chroms.Midpoint, labels=chroms.index)
     ax.set_xticks(chroms.Endpoint, minor=True)
+    ax.set_xlabel("Coordinates")
+    ax.set_ylabel("-$\log_{10}$(p-value)")
 
-    return fig, ax
+    if bool(output):
+        plt.savefig(output, format="png", dpi=150)
+    else:
+        plt.show()
 
 
-@click.command()
-@click.argument("genotypes", type=click.File("r"))
-@click.argument("phenotypes", type=click.File("r"))
-@click.argument("chromosomes", type=click.File("r"))
+@cli.command()
+@click.option("--phenoname", "-p", type=str, default="")
+@click.option("--test", type=click.Choice(assoc_test_dict.keys()), default="anova")
+@click.argument("genotypefile", type=click.Path(exists=True))
+@click.argument("phenotypefile", type=click.Path(exists=True))
 @click.argument("outfile", type=click.File("w"))
-def map_qtls(genotypes, phenotypes, chromosomes, outfile):
-    """Carry out simple QTL mapping for haploid individuals.
+def stats(genotypefile, phenotypefile, outfile, phenoname, test):
+    """
+    Calculate association statistics between genotype and phenotype.
 
     GENOTYPES, PHENOTYPES, and CHROMOSOMES are CSV files. See README for formatting.
 
     OUTFILE is the name of the CSV file to write that contains p-values.
-
     A dash ('-') can be substituted OUTFILE to write to stdout.
     """
-
-    genos = pd.read_csv(genotypes)
-    phenos = pd.read_csv(phenotypes)
-    chroms = pd.read_csv(chromosomes)
-
-    genos.drop(set(genos.columns[2:]) - set(phenos.Sample_Name), axis=1, inplace=True)
-
-    # phenos.drop(set(phenos.Sample_Name) - set(genos.columns[2:]), axis=0, inplace=True)
-
-    qtl_stats = calculate_association_stats(genos, phenos, None)
-    # qtl_stats.to_csv(outfile, index=False)
-
-    # fig, ax = plot_log10pval(qtl_stats, chroms)
-    # plt.show()
+    genodf, phenodf = load_files(genotypefile, phenotypefile)
+    if phenoname == "":
+        phenoname = phenodf.columns[0]
+    assoc = assoc_test_pandarallel(genodf, phenodf, phenoname, genos=(0, 1), test=test)
+    assoc.to_csv(outfile)
 
 
 if __name__ == "__main__":
-    map_qtls()
+    cli()
